f96eaa98e5337578d90242f6cedf93551ec55334
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-22 00:12:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(sentence)
-  sentence_array = []
-  sentence_array << sentence.split(" ")
-  sentence_array_reversed = []
-  sentence_array_reversed << sentence_array.each do |word|
-    word.reverse
-  end
-  return sentence_array_reversed
+#def reverse_each_word(sentence)
+  #sentence_array = []
+ # sentence_array << sentence.split(" ")
+ # sentence_array_reversed = []
+ # sentence_array_reversed << sentence_array.each do |word|
+ #   word.reverse
+ # end
+ # return sentence_array_reversed
 end
